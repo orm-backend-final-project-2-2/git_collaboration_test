2b2af4cc37f2732a2d795377a6922a2f011ee920
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-04 11:28:59 +0900

    ✅ (community): Add failing test

--- community/tests.py
+++ community/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class CommunityTestCase(TestCase):
+    def test_always_success(self):
+        self.assertEqual(True, True)
+
+    def test_always_fail(self):
+        self.assertEqual(True, False)
