824383f6a3c07f30c4b56ae5bc50e39cce7ffce5
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-04 11:52:59 +0900

    ✅ (community): Add commit

--- utils/parse_feature_branch.py
+++ utils/parse_feature_branch.py
@@ -3,11 +3,11 @@ import re
 
 if len(sys.argv) == 2:
     branch_name = sys.argv[1]
-    pattern = re.compile(r'.*feature-(\w+)/(.+)')
+    pattern = re.compile(r".*feature-(.+)/(.+)")
     matches = pattern.match(branch_name)
 
     if matches:
         app_name, feature_name = matches.groups()
-        words = feature_name.split('-')
-        tc_feature_name = ''.join([word.title() for word in words]) + 'TestCase'
-        print(f'{app_name} {tc_feature_name}')
+        words = feature_name.split("-")
+        tc_feature_name = "".join([word.title() for word in words]) + "TestCase"
+        print(f"{app_name} {tc_feature_name}")
