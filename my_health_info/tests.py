229a2bc851054befac10292772e3dbab6c7b049d
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-04 11:40:05 +0900

    ✅ (my_health_info): Add intergration test case
    통합 테스트 추가했어요~

--- my_health_info/tests.py
+++ my_health_info/tests.py
@@ -5,5 +5,6 @@ class DietTestCase(TestCase):
     def test_always_success(self):
         self.assertEqual(True, True)
         
-    def test_always_fail(self):
-        self.assertEqual(True, False)
+class IntergrationTestCase(TestCase):
+    def test_always_success(self):
+        self.assertEqual(True, True)
