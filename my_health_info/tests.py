2be5f80c3ea1e4173ad71e304f7e12a98aa20dc0
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-04 11:19:00 +0900

    ✅ (my_health_info): Add failing test
    항상 실패하는 테스트 추가했어요~

--- my_health_info/tests.py
+++ my_health_info/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class DietTestCase(TestCase):
+    def test_always_success(self):
+        self.assertEqual(True, True)
+        
+    def test_always_fail(self):
+        self.assertEqual(True, False)
