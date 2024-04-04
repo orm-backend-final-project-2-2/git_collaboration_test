8faa6e759690c4280eeeb9286880ceb091134149
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-04 11:40:05 +0900

    ✅ (community) : Add intergration test case

--- community/tests.py
+++ community/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 
 class CommunityTestCase(TestCase):
+
     def test_always_success(self):
         self.assertEqual(True, True)
-
-    def test_always_fail(self):
-        self.assertEqual(True, False)
+        
+class IntergrationTestCase(TestCase):
+    def test_always_success(self):
+        self.assertEqual(True, True)
